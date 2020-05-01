3bd7825e1002e70fab495d3035ae616d4581f264
Ryan Soury <rsoury318@gmail.com>
2020-05-01 16:04:27 +1000

    updated rescripts to set env variables. updated now.json to include env vars and build/route config

--- packages/auth/.rescriptsrc.js
+++ packages/auth/.rescriptsrc.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const { editWebpackPlugin } = require("@rescripts/utilities");
 const get = require("lodash/get");
 const set = require("lodash/set");
 const { alias, jestAlias } = require("./config/alias");
+const pkg = require("./package.json");
 
 const mw = (fn) => Object.assign(fn, { isMiddleware: true });
+
+const env = {
+	PUBLIC_URL: process.env.PUBLIC_URL || ""
+};
 
 const addAlias = mw((config) => {
 	const existingAlias = get(config, "resolve.alias", {});
@@ -21,6 +27,25 @@ const buildApp = mw((config) => {
 		filename: "[name].js",
 		chunkFilename: `[name].[contenthash:8].js`
 	});
+
+	// Setup env vars
+	const definitions = {
+		"process.env.REACT_APP_PUBLIC_URL": JSON.stringify(env.PUBLIC_URL),
+		"process.env.REACT_APP_SENTRY_RELEASE": JSON.stringify(pkg.version)
+	};
+
+	// Apply definitions
+	config = editWebpackPlugin(
+		(p) => {
+			p.definitions = {
+				...p.definitions,
+				...definitions
+			};
+			return p;
+		},
+		"DefinePlugin",
+		config
+	);
 
 	return config;
 });
