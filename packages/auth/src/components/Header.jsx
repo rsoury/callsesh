9852bdde6b93d55ae91f98e3de2288deb2c21057
Ryan Soury <rsoury318@gmail.com>
2020-05-04 02:20:01 +1000

    auth: just returning users to previous page for now when returning to callsesh website.

--- packages/auth/src/components/Header.jsx
+++ packages/auth/src/components/Header.jsx
@@ -10,44 +10,30 @@ import {
 	StyledNavigationList as NavigationList,
 	StyledNavigationItem as NavigationItem
 } from "baseui/header-navigation";
-import { StyledLink } from "baseui/link";
 import ChevronLeft from "baseui/icon/chevron-left";
-import { useStyletron } from "baseui";
 
-import { authConfig } from "@/env-config";
-
-const returnUrl = authConfig.callbackURL;
-
-const Header = () => {
-	const [css] = useStyletron();
-
-	return (
-		<header>
-			<HeaderNavigation
-				overrides={{
-					Root: {
-						style: { borderBottomColor: `transparent` }
-					}
-				}}
-			>
-				<NavigationList $align={ALIGN.left}>
-					<NavigationItem>
-						<StyledLink
-							href={returnUrl}
-							className={css({ textDecoration: "none" })}
-						>
-							<Button
-								kind={BUTTON_KIND.tertiary}
-								startEnhancer={() => <ChevronLeft size={24} />}
-							>
-								Back to Callsesh
-							</Button>
-						</StyledLink>
-					</NavigationItem>
-				</NavigationList>
-			</HeaderNavigation>
-		</header>
-	);
-};
+const Header = () => (
+	<header>
+		<HeaderNavigation
+			overrides={{
+				Root: {
+					style: { borderBottomColor: `transparent` }
+				}
+			}}
+		>
+			<NavigationList $align={ALIGN.left}>
+				<NavigationItem>
+					<Button
+						kind={BUTTON_KIND.tertiary}
+						startEnhancer={() => <ChevronLeft size={24} />}
+						onClick={() => window.history.back()}
+					>
+						Back to Callsesh
+					</Button>
+				</NavigationItem>
+			</NavigationList>
+		</HeaderNavigation>
+	</header>
+);
 
 export default Header;
