cb0e3434168071b97e6452bd9f57451ea8aba125
Ryan Soury <rsoury318@gmail.com>
2020-05-25 13:54:45 +1000

    using chokidar-cli to watch file changes in utils package

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,12 +1,7 @@
-import * as envConfig from "./env-config";
-import * as authManager from "./auth-manager";
 import * as format from "./format";
-import * as comms from "./comms";
 import * as constants from "./constants";
-import * as fees from "./fees";
 
-export { default as stripe } from "./stripe";
 export { default as getRequest } from "./get-request";
 export { default as useRequest } from "./hooks/use-request";
 
-export { envConfig, authManager, format, comms, constants, fees };
+export { format, constants };
