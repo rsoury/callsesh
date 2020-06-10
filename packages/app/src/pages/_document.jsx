c46fb56f6d438c3546d3c00d2dbe706192dc4d11
Ryan Soury <rsoury318@gmail.com>
2020-06-11 01:46:03 +1000

    actually including the manifest in a link html element for browsers to pickup. added orientation to manifest.json

--- packages/app/src/pages/_document.jsx
+++ packages/app/src/pages/_document.jsx
@@ -33,6 +33,7 @@ class Document extends NextDocument {
 						name="viewport"
 						content="width=device-width, initial-scale=1.0"
 					/>
+					<link rel="manifest" href="/manifest.webmanifest" />
 					<link rel="shortcut icon" href="/favicon.ico" />
 					<link
 						rel="apple-touch-icon-precomposed"
