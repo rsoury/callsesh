c3cf273f040393c23689494a2d4c11e7e8bb0dc5
Ryan Soury <rsoury318@gmail.com>
2020-07-29 00:31:51 +1000

    commented logic in chat/auth page to accept a token and send to parent window... however... for some reason our server is not return an authorised user

--- packages/app/src/pages/chat/auth.jsx
+++ packages/app/src/pages/chat/auth.jsx
@@ -4,33 +4,41 @@
  * Post message with login token to parent window.
  */
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import isEmpty from "is-empty";
+// import ono from "@jsdevtools/ono";
 
-import request from "@/utils/request";
-import * as routes from "@/routes";
-import handleException, { alerts } from "@/utils/handle-exception";
-import { chat as config } from "@/env-config";
+// import request from "@/utils/request";
+// import * as routes from "@/routes";
+// import handleException, { alerts } from "@/utils/handle-exception";
+// import { chat as config } from "@/env-config";
 import FullscreenLoader from "@/components/FullscreenLoader";
+// import { getUser } from "@/server/middleware/auth";
+// import * as chat from "@/server/chat";
 
 const ChatAuth = () => {
-	useEffect(() => {
-		request
-			.get(routes.api.chatToken)
-			.then(({ data }) => data)
-			.then(({ token }) => {
-				window.parent.postMessage(
-					{
-						event: "login-with-token",
-						loginToken: token
-					},
-					config.url
-				);
-			})
-			.catch((e) => {
-				handleException(e);
-				alerts.error();
-			});
-	}, []);
+	// useEffect(() => {
+	// 	request
+	// 		.get(routes.api.chatToken, {
+	// 			headers: {
+	// 				Authorization: `Bearer ${token}`
+	// 			}
+	// 		})
+	// 		.then(({ data }) => data)
+	// 		.then(({ token: loginToken }) => {
+	// 			window.parent.postMessage(
+	// 				{
+	// 					event: "login-with-token",
+	// 					loginToken
+	// 				},
+	// 				config.url
+	// 			);
+	// 		})
+	// 		.catch((e) => {
+	// 			handleException(e);
+	// 			alerts.error();
+	// 		});
+	// }, []);
 
 	return (
 		<FullscreenLoader>
@@ -38,5 +46,28 @@ const ChatAuth = () => {
 		</FullscreenLoader>
 	);
 };
+
+// export async function getServerSideProps({ req }) {
+// 	try {
+// 		// Get currently authed user
+// 		const user = await getUser(req, { withContext: true });
+// 		console.log(user);
+
+// 		// Login to chat service and get encrypted password
+// 		const { data = {} } = await chat.login(
+// 			user.username,
+// 			user.chatUser.password
+// 		);
+// 		const token = data.authToken || "";
+
+// 		return {
+// 			props: {
+// 				token
+// 			}
+// 		};
+// 	} catch (e) {
+// 		return { props: {} };
+// 	}
+// }
 
 export default ChatAuth;
