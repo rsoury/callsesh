c3fab6ee6005799655d73db1c5c41186a3a6b31e
Ryan Soury <rsoury318@gmail.com>
2020-08-10 23:01:48 +1000

    increased message broadcast length on onboarding/profile-settings fields

--- packages/app/src/frontend/components/Onboarding/OperatorStep.jsx
+++ packages/app/src/frontend/components/Onboarding/OperatorStep.jsx
@@ -316,7 +316,7 @@ const OperatorStep = ({ noToggle, values }) => {
 								name="messageBroadcast"
 								label="Have a message for your callers? Could be your biography, or a note about your calls"
 								placeholder="Hey! One thing you should know about me is..."
-								maxLength={240}
+								maxLength={480}
 								area
 								overrides={{
 									Input: {
